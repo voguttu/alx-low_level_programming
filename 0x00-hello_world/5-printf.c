37aba56f880788fd849d45499707eac6c19c0217
voguttu <voguttu@gmail.com>
2023-02-20 17:15:38 +0300

    5. Hello, printf-FIX

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'Prints exactly with proper grammar,
+  * but the outcome is a piece of art, followed by new line'
+  *
+  * Return: Always 0 (Success)
+  */
 
-int main(void)		/* Entry point main function */
+int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
