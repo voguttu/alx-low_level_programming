a2a56f94ff6211f3d50f8a015b8cc14b2f058ef9
voguttu <voguttu@gmail.com>
2023-02-20 15:43:27 +0300

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-/* main function */
-int main(void)/*
+/**
+  * main - Entry point
+  *
+  * Description: 'C Program that prints exactly "Programming
+  * is like building a multilingual puzzle'
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
 {
-	/* string initialization */
 	char hello[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(hello);
-
 	return (0);
 }
