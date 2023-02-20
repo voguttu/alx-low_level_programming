b95958b04c5d9cbc3f9e521cefb1e4e7e396bb8d
voguttu <voguttu@gmail.com>
2023-02-20 18:05:43 +0300

    4. Hello, puts-FIX 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'C program that prints exactly and that
+  * piece of art is useful" - Dora Korpar, 2015-10-19,
+  * followed by a new line, to the standard error'
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		return (1);
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char hello[] = "\"Programming is like building a multilingual puzzle\n";
+	char hello[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(hello);
 	return (0);
