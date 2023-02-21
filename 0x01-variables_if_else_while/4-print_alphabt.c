28a9fcf3f714cdd88eb387c356a48d92dd6b358a
voguttu <voguttu@gmail.com>
2023-02-21 09:08:30 +0300

    4. Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'program that prints the alphabet
+ * in lowercase except q and e, followed by a new line'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	while (a < 123 && a != 131)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
