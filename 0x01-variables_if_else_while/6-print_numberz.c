d4320f082be26df21574e2a9c0154c74d6213f57
voguttu <voguttu@gmail.com>
2023-02-21 07:27:48 +0300

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program that prints all single digit numbers
+ * of base 10 starting from 0'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(48);
+	  putchar(49);
+	   putchar(50);
+	    putchar(51);
+	     putchar(52);
+	      putchar(53);
+	       putchar(54);
+	       putchar(55);
+	       putchar(56);
+	       putchar(57);
+	       putchar(10);
+	return (0);
+}
