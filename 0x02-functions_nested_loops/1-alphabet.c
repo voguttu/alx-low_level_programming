c39640aed4c54452525215ddeadde2e20f44d0b9
voguttu <voguttu@gmail.com>
2023-02-25 13:51:24 +0300

    1. Insomnia -FIX

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -17,5 +17,4 @@ void print_alphabet(void)
 		lca++;
 	}
 	_putchar('\n');
-	return (lca);
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower -function that checks for lowercase character
+ * @parameterc -variable declaration type int
+ *
+ * Return: On success 1.
+ * Otherwise 0 is returned
+ */
+
+int _islower(int c)
+{
+	int lch;
+
+	lch = _islower(c);
+	return (write(1, &c, 1));
+}
