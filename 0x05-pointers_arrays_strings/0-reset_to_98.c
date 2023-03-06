414421405601a5cf457b8c7036f4a8108dea4d6b
voguttu <voguttu@gmail.com>
2023-03-06 07:56:16 +0300

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - function updating value to 98
+  *
+  * @n: variable reset value
+  *
+  * Return: Void
+  *
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
