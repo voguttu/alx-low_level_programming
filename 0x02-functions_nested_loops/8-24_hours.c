67b38598a01355c1b9dd32566e6fe394824cfba0
voguttu <voguttu@gmail.com>
2023-03-08 09:34:41 +0300

    8. Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+  * jack_bauer - function that prints every minute
+  *
+  * Return: void
+  */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		if (a != 2 && a < 2)
+		{
+			for (b = 0; b <= 9; b++)
+			{
+				for (c = 0; c <= 5; c++)
+				{
+					for (d = 0; d <= 9; d++)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+else if (a == 2)
+{
+	for (b = 0; b <= 4; b++)
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	if (b != 4 && b < 4)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+	}}}}}}
+}
