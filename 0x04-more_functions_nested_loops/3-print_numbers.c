252b94b446d1dc44afa2beda6c264ed1c2c670c3
voguttu <voguttu@gmail.com>
2023-03-08 17:11:00 +0300

    3. The numbers speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_numbers - function that prints the
+  * numbers, from 0 to 9
+  *
+  * Return: Void
+  */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
