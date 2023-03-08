8c28696b5f2f64ff7943f502d609d523fc55fa51
voguttu <voguttu@gmail.com>
2023-03-08 13:00:13 +0300

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * add - function that adds two integers
+  *
+  * @a: first member
+  * @b: second member
+  *
+  * Return: returns result
+  */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * times_table - function that prints the 9 times table
+  *
+  * Return: Void
+  */
+
+void times_table(void)
+{
+	
+
+
+}
