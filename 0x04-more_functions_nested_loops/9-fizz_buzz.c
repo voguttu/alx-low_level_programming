af7e2215818dd60787f18008ca4b6f6be976589b
voguttu <voguttu@gmail.com>
2023-03-09 15:39:39 +0300

    9. Fizz-Buzz-FIX

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,17 +18,17 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if ((a % 3) == 0)
+		if ((a % 3) == 0 && (a % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((a % 3) == 0)
 		{
 			printf("Fizz ");
 		}
 		else if ((a % 5) == 0)
 		{
 			printf("Buzz ");
-		}
-		else if ((a % 3) == 0 && (a % 5) == 0)
-		{
-			printf("Fizz Buzz ");
 		}
 		else
 		{
