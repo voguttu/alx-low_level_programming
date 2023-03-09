477d3b16d40bb602a17ba278d6e83df5026edf7a
voguttu <voguttu@gmail.com>
2023-03-09 19:21:52 +0300

    Git Restored

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function checking the length of a string
+ *
+ * @s: string to checked
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	char *str;
+	
+	str = &s;
+
+	return (str);
+}
