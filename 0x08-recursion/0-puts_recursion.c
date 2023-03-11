2cb5390941bdd4e92c16e50875a75366bb8b38a5
voguttu <voguttu@gmail.com>
2023-03-11 13:19:23 +0300

    0. Recurssion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - function that prints a string
+  *
+  * @s: Member
+  *
+  * Return: Void
+  */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+	{
+	_puts_recursion(s);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
