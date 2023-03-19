d4a914511d3ef0ca8f8a2118b99c3e69f3ea53a7
voguttu <voguttu@gmail.com>
2023-03-19 19:55:55 +0300

    5. Reverse String-FIX2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,11 +16,10 @@ void rev_string(char *s)
 	int a, my_sl;
 
 	my_sl = _strlen(s);
-	for (a = my_sl; a >= 0; a--)
+	for (a = my_sl - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
-	_putchar('\n');
 }
 /**
  * _strlen - function that counts string length
