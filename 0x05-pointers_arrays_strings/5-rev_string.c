5662f81d9a2ffc96dabe0bdf5081dcb528a44da5
voguttu <voguttu@gmail.com>
2023-03-19 19:39:09 +0300

    5. Reverse String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @*s: String to be reversed
+ * @s: string characters to be counted
+ *
+ * Return: Void
+ */
+
+int _strlen(char *s);
+
+void rev_string(char *s)
+{
+	int a, my_sl;
+
+	my_sl = _strlen(s);
+	for (a = my_sl - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen - function that counts string length
+ *
+ * @s: string characters to be counted
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
