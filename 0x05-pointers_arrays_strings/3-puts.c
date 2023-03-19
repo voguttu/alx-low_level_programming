72c0d94df07261cc10d92d1e5145d195489a1d78
voguttu <voguttu@gmail.com>
2023-03-19 17:25:11 +0300

    3. Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ *
+ * _strlen - function that counts string length
+ *
+ * @*str: String to be printed
+ * @s: characters to be counted
+ *
+ * Return: Void
+ */
+
+int _strlen(char *s);
+
+void _puts(char *str)
+{
+	int a, my_sl;
+
+	my_sl = _strlen(str);
+	for (a = 0; a < my_sl; a++)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
