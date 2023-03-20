7ab94e4e9ac5aab636fdaafce29acae3e284792a
voguttu <voguttu@gmail.com>
2023-03-20 09:15:13 +0300

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
+int _putchar(char c);
 /**
   * char *_strcpy - function that copies the string pointed
   *
-  * @*dest: First Member
+  * @dest: First Member
   * @src: Second member
   *
   * Return: the pointer to dest
@@ -11,12 +12,31 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int a;
+	int i;
 
-	for (a = 0; a <= '\0'; a++)
+	for (i = 0; i <= '\0'; i++)
 	{
-		src[a] = dest[a];
+		dest[i] = src[i - 1];
 	}
+	dest = src;
+	for (i = 0; i < '\0'; i++)
+	{
+		_putchar(dest[i]);
+		dest++;
+	}
+	return (dest); 
 
-	return(*dest:wq:wqw);
+}
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
