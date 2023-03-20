752b69bbd6c2fad0582fc8fafca23c9898394eac
voguttu <voguttu@gmail.com>
2023-03-20 09:16:27 +0300

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-int _putchar(char c);
 /**
   * char *_strcpy - function that copies the string pointed
   *
@@ -26,17 +25,4 @@ char *_strcpy(char *dest, char *src)
 	}
 	return (dest); 
 
-}
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
