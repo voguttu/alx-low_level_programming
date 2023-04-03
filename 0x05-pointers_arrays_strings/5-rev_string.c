169c8cca3ac9ad0bcd805f08c5161f168df92a69
voguttu <voguttu@gmail.com>
2023-04-03 17:17:18 +0300

    5. Reverse String

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,13 @@ void rev_string(char *s)
 	int a, my_sl;
 
 	my_sl = _strlen(s);
-	for (a = my_sl - 1; a >= 0; a--)
+	for (a = my_sl; a >= 0; a--)
 	{
-		_putchar(s[a]);
 		s[a] = s[a - 1];
+		if (a != '\0')
+		{
+		_putchar(s[a]);
+		}
 	}
 }
 /**
