daf67a0128e6927c62a5cc21efa8c712d98847c5
voguttu <voguttu@gmail.com>
2023-05-24 20:06:02 +0300

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,25 @@
 
 void times_table(void)
 {
+	int a;
+	int b;
+	int c;
 	
-
-
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			_putchar('0' + c);
+			if (b <= 8)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }
