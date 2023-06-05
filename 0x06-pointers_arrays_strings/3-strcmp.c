4f7481f7f9f07553969b9ffeb2e66774f4c4e3a8
voguttu <voguttu@gmail.com>
2023-06-05 15:01:15 +0300

    String Compare fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,54 +1,29 @@
 #include "main.h"
 
 /**
-  * _strcmp - function that compares two strings
-  *
-  * @s1: 1st Member
-  * @s2: 2nd member
-  *
-  * Return: (-ve) if s1 < s2
-  * (+ve) if s1 > s2
-  * (0) if s1 == s2
-  *
-  */
+ * _strcmp - function that compares two strings
+ *
+ * @s1: 1st Member
+ * @s2: 2nd member
+ *
+ * Return: value (*s1 - *s2)
+ *
+ */
 
 int _strcmp(char *s1, char *s2)
 {
-	int a, b;
+	int result;
+	char *a;
+	char *b;
 
-	a = _strlen(s1);
-	b = _strlen(s2);
+	a = s1;
+	b = s2;
+	result = *a - *b;
 
-	if (a < b)
+	while (*a && (*a == *b))
 	{
-		return (-15);
+		a++;
+		b++;
 	}
-	else if (a > b)
-	{
-		return (15);
-	}
-	else if (a == b)
-	{
-		return (0);
-	}
-	return (0);
-}
-/**
- * _strlen - function checking the length of a string
- *
- * @s: string to checked
- *
- * Return: Always 0.
- */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
+	return (result);
 }
