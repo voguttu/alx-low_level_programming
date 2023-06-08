9eb9322b5744bd6b8ff054bad4c4c8aa733b1671
voguttu <voguttu@gmail.com>
2023-06-08 14:37:06 +0300

    Conflict resolution

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _strcmp - function that compares two strings
+  *
+  * @s1: First member
+  * @s2: Second member
+  *
+  * Return: returns zero if strings are the same
+  * otherwise returns less than or greater than zero 
+  * if strings are not similar
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int count;
+	char str1;
+	char str2;
+
+	str1 = s1;
+	str2 = s2;
+
+	if (str[a] != '\0' && str2[a] != '\0')
+	{
+		for (count = 0; str1[a] != str2[a]; a++)
+	{
+		count++;
+	}
+		return (0);
+	}
+	else if (s1 < s2)
+	{
+		return (count)
