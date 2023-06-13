b9933c9b13d02908681556545b98c426e576a58c
voguttu <voguttu@gmail.com>
2023-06-13 05:34:07 +0300

    Print Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 		_putchar('\n');
 		return;
 	}
-	for (i = 1; i <= size; a++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= ii; j++)
+		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 		}
