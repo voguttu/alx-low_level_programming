17bd7657d706c619583f96c8c75448069c13611c
voguttu <voguttu@gmail.com>
2023-06-14 10:44:27 +0300

    Static Library

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,6 +1,49 @@
 #include "main.h"
 
+/**
+  * _strncpy - function that copies a string
+  *
+  * @src: String to be copied
+  * @dest: Copied string
+  * @n: number of bytes  to be copied
+  *
+  * Return: pointer to the copied string
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
+	int a, n1;
+	char *str1;
 
+	n1 = _strlen(src);
+	str1 = src;
+
+	for (a = 0; a < n && a <= n1; a++)
+	{
+		dest[a] = str1[a];
+	}
+	for (; a < n; a++)
+	{
+		dest[a] = '\0';
+	}
+	return (dest);
+}
+/**
+ * _strlen - function checking the length of a string
+ *
+ * @s: string to checked
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
+/**
+ * _strcmp - function that compares two strings
+ *
+ * @s1: 1st Member
+ * @s2: 2nd member
+ *
+ * Return: value (*s1 - *s2)
+ *
+ */
+
 int _strcmp(char *s1, char *s2)
 {
+	char *a;
+	char *b;
 
+	a = s1;
+	b = s2;
+
+	while (*a && (*a == *b))
+	{
+		a++;
+		b++;
+	}
+	return (*a - *b);
 }
