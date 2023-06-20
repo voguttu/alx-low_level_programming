35d9e7ebeb410aa0d7f42d76e6bf21560f3db21d
voguttu <voguttu@gmail.com>
2023-06-20 19:09:11 +0300

    Recussion Prime Number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int is_prime(int n, int tester);
 /**
   * is_prime_number - function that checks if input integer
   * is a prime number.
@@ -12,28 +13,34 @@
 
 int is_prime_number(int n)
 {
-	int a, count;
-
-	a = 1;
-	count = 0;
-
-	if ((n % a) == 0)
+	if (n <= 1)
 	{
-		count++;
-		a++;
-		if (a <= n)
-		{		
-			is_prime_number(n);
-		}
-	}
-	if (count > 2)
-	{
-		_putchar('\n');
 		return (0);
 	}
 	else
 	{
-		_putchar('\n');
+		return (is_prime(n, 2));
+	}
+}
+/**
+ * is_prime - helper function that checks if input integer
+ * is a prime number.
+ *
+ * @n: Member
+ * @tester: number used to check if integer is divisible
+ * Return: returns 1 if the input integer is a prime number
+ * otherwise return 0
+ */
+
+int is_prime(int n, int tester)
+{
+	if ((tester * tester) > n)
+	{
 		return (1);
 	}
+	if ((n % tester) == 0)
+	{
+		return (0);
+	}
+	return (is_prime(n, tester + 1));
 }
