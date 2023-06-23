538ed508967722e6caa4be548957965aa66208d1
voguttu <voguttu@gmail.com>
2023-06-23 16:22:46 +0300

    Double Pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - function that sets the value of a pointer to a char
+ * @s: string
+ * @to: new String
+ *
+ * Return: Void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
