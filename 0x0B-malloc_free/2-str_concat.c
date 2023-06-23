c6ecfdfd11c5a846e247da47309ace2951666539
voguttu <voguttu@gmail.com>
2023-06-23 20:02:23 +0300

    Malloc str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,46 +8,38 @@ int _strlen(char *s);
   * @s1: first member
   * @s2: second member
   *
-  * Return: Concsted string
+  * Return: Concated string
   * Otherwise return NULL on failure
   */
 
 char *str_concat(char *s1, char *s2)
 {
-	int a, b, n1, n2;
-	char *p1;
-	char *p2;
+	int a, b, n1, n2, n3;
 	char *p3;
 
 	if (s1 == NULL || s2 == NULL)
 	{
 		return (NULL);
 	}
-	else
+
+	n1 = _strlen(s1);
+	n2 = _strlen(s2);
+	n3 = n1 + n2;
+	p3 = malloc(sizeof(char) * (n3 + 1));
+
+	if (p3 == NULL)
 	{
-		n1 = _strlen(s1);
-		n2 = _strlen(s2);
-
-		p1 = malloc(sizeof(s1) * n1);
-		p1 = s1;
-
-		p2 = malloc(sizeof(s2) * n2);
-		p2 = s2;
-
-		p3 = malloc((sizeof(s2) * n1) + (sizeof(s2) * n2));
-
-		for (a = 0; a < n1; a++)
-		{
-			p3[b] = p1[a];
-			b++;
-		}
-		for (a = 0; a < n2; a++)
-		{
-			p3[b] = p2[a];
-			b++;
-		}
-		p3[b] = '\0';
+		return (NULL);
 	}
+	for (a = 0; a < n1; a++)
+	{
+		p3[a] = s1[a];
+	}
+	for (b = 0; b < n2; b++)
+	{
+		p3[a + b] = s2[b];
+	}
+	p3[a + b] = '\0';
 	return (p3);
 }
 /**
