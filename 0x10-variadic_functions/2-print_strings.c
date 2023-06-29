2d7a37838560681afc42b595dda688858318a227
voguttu <voguttu@gmail.com>
2023-06-29 10:03:22 +0300

    Print Strings - Variadic Functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(s_print, const char *);
 		if (str == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 		{
