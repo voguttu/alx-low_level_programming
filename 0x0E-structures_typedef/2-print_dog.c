1b8121ed0fa905b6f46b00123e3b67e77296bbc5
voguttu <voguttu@gmail.com>
2023-07-04 10:09:43 +0300

    2.Print struct - Fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 		exit(98);
 	}
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
