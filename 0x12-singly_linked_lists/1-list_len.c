c9bfd65508dc19ad3bf3834f938045cbc86f8590
voguttu <voguttu@gmail.com>
2023-07-05 05:54:48 +0300

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in linked list
+ * @h: member
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
